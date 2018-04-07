24c3e8e51ee479714a0f8ce02fbfb238f2880a07
dhinesh1206 <dhinesh_vinayak@yahoo.co.in>
2018-04-07 18:26:59 +0530

    Added Main Screen WIth Few IN/Out Animations

---
+++ Assets/_Scripts/CostumeSlection.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CostumeSlection : MonoBehaviour {
+
+	public int hairIndex = 0;
+	public int clothIndex = 0;
+	public int costumeIndex = 0;
+
+	public bool hairSelected;
+	public bool clothSelected;
+	public bool fullConstume;
+
+	public PlaneFinderBehaviour planefinder;
+	public Renderer fullbody;
+	public Renderer uiBody;
+
+	public Material dark;
+
+	public CharecterStyle styles;
+	public CharecterStyle maincharecter;
+	// Use this for initialization
+
+	void Start () {
+		//StartCoroutine (randomSelection ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator randomSelection() {
+		yield return new WaitForSeconds (Random.Range (0, 3));
+		maincharecter.fullConstume [costumeIndex].SetActive (false);
+		int num = Random.Range (0, 3);
+		costumeIndex = num;
+		maincharecter.fullConstume [costumeIndex].SetActive (true);
+		StartCoroutine (randomSelection ());
+
+	}
+
+	public void Select() {
+		planefinder.enabled = true;
+	}
+
+	public void Next()
+	{
+		if (hairSelected == true) {
+			styles.hair [hairIndex].SetActive (false);
+			maincharecter.hair [hairIndex].SetActive (false);
+			hairIndex = hairIndex + 1 != styles.hair.Count ? hairIndex + 1 : 0; 
+			maincharecter.hair [hairIndex].SetActive (true);
+			styles.hair [hairIndex].SetActive (true);
+		} else if (clothSelected == true) {
+			styles.Dress [clothIndex].SetActive (false);
+			maincharecter.Dress [clothIndex].SetActive (false);
+			clothIndex = clothIndex + 1 != styles.Dress.Count ? clothIndex + 1 : 0; 
+			styles.Dress [clothIndex].SetActive (true);
+			maincharecter.Dress [clothIndex].SetActive (true);
+		} else if(fullConstume == true) {
+			styles.fullConstume [costumeIndex].SetActive (false);
+			maincharecter.fullConstume [costumeIndex].SetActive (false);
+			costumeIndex = costumeIndex + 1 != styles.fullConstume.Count ? costumeIndex + 1 : 0; 
+			fullbody.material = maincharecter.bodyMaterial [costumeIndex];
+			uiBody.material = maincharecter.bodyMaterial [costumeIndex];
+			styles.fullConstume [costumeIndex].SetActive (true);
+			maincharecter.fullConstume [costumeIndex].SetActive (true);
+		}
+	}
+
+	public void Previous()
+	{
+		if (hairSelected == true) {
+			styles.hair [hairIndex].SetActive (false);
+			maincharecter.hair [hairIndex].SetActive (false);
+			hairIndex = hairIndex - 1 < 0 ? hairIndex - 1 : styles.hair.Count -1; 
+			maincharecter.hair [hairIndex].SetActive (true);
+			styles.hair [hairIndex].SetActive (true);
+		} else if (clothSelected == true) {
+			styles.Dress [clothIndex].SetActive (false);
+			maincharecter.Dress [clothIndex].SetActive (false);
+			clothIndex = clothIndex - 1 < 0 ? clothIndex - 1 : styles.Dress.Count -1; 
+			styles.Dress [clothIndex].SetActive (true);
+			maincharecter.Dress [clothIndex].SetActive (true);
+		} else if(fullConstume == true) {
+			styles.fullConstume [costumeIndex].SetActive (false);
+			maincharecter.fullConstume [costumeIndex].SetActive (false);
+			costumeIndex = costumeIndex - 1 > -1 ? costumeIndex - 1 : styles.fullConstume.Count -1; 
+			fullbody.material = maincharecter.bodyMaterial [costumeIndex];
+			uiBody.material = maincharecter.bodyMaterial [costumeIndex];
+			styles.fullConstume [costumeIndex].SetActive (true);
+			maincharecter.fullConstume [costumeIndex].SetActive (true);
+		}
+	}
+		
+}
+	
+[System.Serializable]
+public class CharecterStyle
+{
+	public List<GameObject> Dress;
+	public List<GameObject> hair;
+	public List<GameObject> fullConstume;
+	public List<Material> bodyMaterial;
+}
---
+++ Assets/_Scripts/UI/UIManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class UIManager : MonoBehaviour {
+	public RectTransform[] SideBtns;
+	public float sideBtnAnimTime;
+	bool isExpanded;
+
+	public List<ListScreens> screens;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnSideBtnClick(){
+		if (isExpanded)
+			StartCoroutine(ShrinkSide ());
+		else
+			StartCoroutine(ExpandSide ());
+	}
+
+
+	IEnumerator ExpandSide(){
+		isExpanded = true;
+		for (int i = 0; i < SideBtns.Length; i++) {
+			SideBtns[i].DOScale (Vector3.one, sideBtnAnimTime).SetEase (Ease.OutBack);
+			yield return new WaitForSeconds (sideBtnAnimTime/2);
+		}
+	}
+
+
+	IEnumerator ShrinkSide(){
+		isExpanded = false;
+		for (int i = SideBtns.Length; i > 0; i--) {
+			SideBtns[i-1].DOScale (Vector3.zero, sideBtnAnimTime).SetEase (Ease.Linear);
+			yield return new WaitForSeconds (sideBtnAnimTime/2);
+		}
+	}
+
+	public void OnscreenSelection(int ButtonNumber) {
+		foreach (var screen in screens) {
+			if (screen.buttonNumber == ButtonNumber) {
+				ExpandScreen (ButtonNumber, screen.ScreenTarget);
+			}
+		}
+	}
+
+	public void ExpandScreen(int buttonnumber,GameObject Target) {
+		SideBtns[buttonnumber].DOScale(new Vector3(6,6,6), 0.5f).SetEase(Ease.Linear).OnComplete(() => {
+			Target.SetActive(true);
+			InstantShrink();
+		});
+	}
+
+	public void InstantShrink() {
+		isExpanded = false;
+		for (int i = SideBtns.Length; i > 0; i--) {
+			SideBtns[i-1].DOScale (Vector3.zero,0).SetEase (Ease.Linear);
+		}
+	}
+}
+
+[System.Serializable]
+
+public class ListScreens{
+	public int buttonNumber;
+	public GameObject ScreenTarget;
+
+}
